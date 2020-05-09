f6a9df67ddb949a26ad962f99b4aa7e5b7d1f8ca
Rafael Nascimento <nasrafael@gmail.com>
2020-05-09 16:26:03 -0300

    Includes passport, config and jwt packages to packages json

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { Logger } from "@nestjs/common";
-import * as config from "config";
+import * as config from 'config';
 
 async function bootstrap() {
   const serverConfig = config.get("server");
